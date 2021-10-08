d6b100a74a6b5949ffd0d294cd635d68ed086ad1
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-10-07 22:30:58 -0300

    ⬅️ response

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, request
+from flask.wrappers import Response
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return "<a href='/posts'>Posts</a>"
+
+
+@app.route('/response')
+def response():
+    return 'Uma resposta do servidor'
+
 
 @app.route('/posts')
 @app.route('/posts/<int:id>')
@@ -12,15 +20,16 @@ def posts(id):
     titulo = request.args.get('titulo')
 
     data = dict(
-        path = request.path,
-        referrer = request.referrer,
-        content_type = request.content_type,
-        method = request.method,
-        titulo = titulo,
-        id = id if id else 0
+        path=request.path,
+        referrer=request.referrer,
+        content_type=request.content_type,
+        method=request.method,
+        titulo=titulo,
+        id=id if id else 0
     )
 
     return data
+
 
 if __name__ == '__main__':
     app.run()
