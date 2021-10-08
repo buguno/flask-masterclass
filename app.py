b8530b577703978065559c8533a73e6e70d6c7bd
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-10-07 22:15:48 -0300

    ➡️ requests na prática

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Página inicial'
+    return "<a href='/posts'>Posts</a>"
+
+@app.route('/posts')
+@app.route('/posts/<int:id>')
+def posts(id):
+    titulo = request.args.get('titulo')
+
+    data = dict(
+        path = request.path,
+        referrer = request.referrer,
+        content_type = request.content_type,
+        method = request.method,
+        titulo = titulo,
+        id = id if id else 0
+    )
+
+    return data
 
 if __name__ == '__main__':
     app.run()
